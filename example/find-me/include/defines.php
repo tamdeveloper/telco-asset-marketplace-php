701e1b4f688b903a833d6e0e634fa6a68eb5fde7
mikhaelharswanto <mikhael.harswanto@gmail.com>
2011-04-11 11:18:10 +0700

    modified variable definitions for example apps

--- example/find-me/include/defines.php
+++ example/find-me/include/defines.php
@@ -2,14 +2,14 @@
 
 define("SSL_VERIFIER", false); // set this to true for production use
 
-define("APP_HOST", "http://localhost");
-define("APP_URL", APP_HOST . "/telco-asset-marketplace-php/example/find-me");
+define("APP_HOST", "http://yoursite.com");
+define("APP_URL", APP_HOST . "/find-me");
 define("APP_CALLBACK_URL", APP_URL . "/callback.php");
 
-define("TAM_CONSUMER_KEY", "j6umi2wryijlsqf5"); // this is your application consumer key
-define("TAM_CONSUMER_SECRET", "zp50ulo1q92txg92"); // this is your application consumer secret
+define("TAM_CONSUMER_KEY", "your_consumer_key"); // this is your application consumer key
+define("TAM_CONSUMER_SECRET", "your_consumer_secret"); // this is your application consumer secret
 
-define("TAM_OAUTH_HOST", "https://localhost:9443");
+define("TAM_OAUTH_HOST", "https://telcoassetmarketplace.com");
 define("TAM_REQUEST_TOKEN_URL", TAM_OAUTH_HOST . "/api/1/oauth/request_token");
 define("TAM_AUTHORIZE_URL", TAM_OAUTH_HOST . "/web/authorize");
 define("TAM_ACCESS_TOKEN_URL", TAM_OAUTH_HOST . "/api/1/oauth/access_token");
