5c1d84356feaadde1d8ec92652d4be5c5e5aceb0
mikhaelharswanto <mikhael.harswanto@gmail.com>
2011-04-27 14:07:28 +0700

    updated common lib

--- example/sms-hello-world/include/defines.php
+++ example/sms-hello-world/include/defines.php
@@ -6,8 +6,8 @@ define("APP_HOST", "http://yoursite.com");
 define("APP_URL", APP_HOST . "/telco-asset-marketplace-php/example/sms-hello-world");
 define("APP_CALLBACK_URL", APP_URL . "/callback.php");
 
-define("TAM_CONSUMER_KEY", "5yzbcctx4palgrqt"); // this is your application consumer key
-define("TAM_CONSUMER_SECRET", "r90hibida9yb72gj"); // this is your application consumer secret
+define("TAM_CONSUMER_KEY", "your_consumer_key"); // this is your application consumer key
+define("TAM_CONSUMER_SECRET", "your_consumer_secret"); // this is your application consumer secret
 
 define('OAUTH_TMP_DIR', function_exists('sys_get_temp_dir') ? sys_get_temp_dir() : realpath($_ENV["TMP"]));
 
